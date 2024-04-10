18509b3630a2e56fe5bfdc90eda0ebebfe4740a1
Artyom Chshyogolev <artyom.chshyogolev@gmail.com>
2024-04-10 09:13:39 +0500

    Update index.ts

--- index.ts
+++ index.ts
@@ -9,7 +9,7 @@ import compression from "compression"
 config()
 
 const app = express()
-const port = 8000
+const port = process.env.PORT || 8080
 let reqCount = 0
 let lastTime = Date.now()
 let totalRequestsPerMinute = 0
